b4434ed3b68beadf66e342af78b8e3ca6bd2d446
csfreak92 <ralph.andalis92@gmail.com>
2013-04-22 11:02:41 +0800

    Updated node.js

---
+++ node.js
@@ -0,0 +1,8 @@
+// Sample Node program 
+var http = require("http");
+
+http.createServer(function(request,response){ 
+  response.writeHead(200, {"Content-Type":"text/plain"});
+  response.write("Welcome to Node.js!");
+  response.end();
+}).listen(8888);
